fb1059570c6f4f4f66569fa95990be6e9e024f81
Saurabh <s.yadav0791@gmail.com>
2017-04-09 02:41:36 +0530

    added maps and setting part, need to add the custom slide button

--- LocationLoktra/ViewController.swift
+++ LocationLoktra/ViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapviewLocation: MKMapView!
+    var currentLocation : CLLocation?
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        isAuthorizedtoGetUserLocation()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestLocation()
+        }
     }
-
-
+    
+    //if we have no permission to access user location, then ask user for permission.
+    func isAuthorizedtoGetUserLocation() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse     {
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: "Alert", message: "Please enable location service in the Settigs app under Privacy, Location Services. To get the location.", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in
+                }))
+                alert.addAction(UIAlertAction(title: "Settings", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in
+                    UIApplication.shared.open(NSURL(string:UIApplicationOpenSettingsURLString)! as URL, options: [:], completionHandler: nil)
+                    self.locationManager.startUpdatingLocation()
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    //this method will be called each time when a user change his location access preference.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            print("User allowed us to access location")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Did location updates is called")
+        let location = locations.last! as CLLocation
+        currentLocation = location
+        locationManager.stopUpdatingLocation()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        mapviewLocation.showsUserLocation = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Did location updates is called but failed getting location \(error)")
+    }
+    
+    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus)
+    {
+        if CLLocationManager.locationServicesEnabled() {
+            if status == .denied || status == .restricted {
+                DispatchQueue.main.async
+                    {
+                        let alert = UIAlertController(title: "Alert", message: "Please enable location service in the Settigs app under Privacy, Location Services.", preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in
+                        }))
+                        alert.addAction(UIAlertAction(title: "Settings", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in
+                            UIApplication.shared.open(NSURL(string:UIApplicationOpenSettingsURLString)! as URL, options: [:], completionHandler: nil)
+                        }))
+                        self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
 }
-
